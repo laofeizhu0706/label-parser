7cecae3ff821f457c865755d38a857ab9b4a592e
laofeizhu <1342439191@qq.com>
2021-02-05 22:50:21 +0800

    fix(sh):fix cp bug

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ jar_name=label-service-1.0
 pid=$(ps -ef|grep ${jar_name}|grep -v grep|awk '{print $2}')
 if [ -n "${pid}" ];then
     kill -9 $pid
-    print_log "存在${jar_name}的进程 ${pid}"
+    log "存在${jar_name}的进程 ${pid}"
 fi
 rm -f ./label-service-1.0.jar
 cp ./label-service/target/label-service-1.0.jar .
@@ -29,7 +29,7 @@ jar_name=label-admin-web
 pid=$(ps -ef|grep ${jar_name}|grep -v grep|awk '{print $2}')
 if [ -n "${pid}" ];then
     kill -9 $pid
-    print_log "存在${jar_name}的进程 ${pid}"
+    log "存在${jar_name}的进程 ${pid}"
 fi
 rm -f ./label-admin-web.jar
 cp ./label-admin-parent/label-admin-web/target/label-admin-web.jar .
@@ -40,7 +40,7 @@ log "end label-admin-web.jar"
 # 部署  label-admin-vue
 log "start label-admin-vue"
 rm -rf ${NGINX_STATIC_DATA_PATH}/dist
-cp ./label-admin-parent/label-admin/dist ${NGINX_STATIC_DATA_PATH}/
+cp -r ./label-admin-parent/label-admin/dist ${NGINX_STATIC_DATA_PATH}/
 log "end label-admin-vue"
 
 
